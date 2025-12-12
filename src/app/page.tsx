a1183af531acfd831e5174b36b6d3b7789d04362
Ammar Rashid <ammar.rashid221@gmail.com>
2025-12-12 20:37:13 +0700

    update final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
-import Weather from '@/components/Weather';
+import { GetWeather } from '@/Libs/api-libs';
+import Image from 'next/image';
 
-export default function Home() {
+export default async function Home() {
+
+  const wather = await GetWeather({ city: `jakarta`});
+
   return (
-    <>
-      <Weather city={'yogyakarta'}/>
-    </>
-    
+    <div>
+      <h1 className='text-xl font-bold'>{wather.location.name},{wather.location.region}</h1>
+      <h1 className='text-5xl font-bold'>{wather.current.temp_c}</h1>
+      <Image src={`https:${wather.current.condition.icon}`} alt='weather icon' width={100} height={100} />
+      <h1 className='text-xl font-bold'>{wather.current.condition.text}</h1>
+    </div>
+      
   );
 }
